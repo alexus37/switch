64ddafaefdf55070429b479d10151a85e2f89166
alexus37 <alex.lexus.info@gmail.com>
2021-07-19 22:42:46 +0200

    first working version

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Switch
 
 This project controls the switch of the attic.
-The first chip is an esp8266 https://arduino-esp8266.readthedocs.io/en/latest/installing.html
+The first chip is an esp32 firebettle https://arduino-esp8266.readthedocs.io/en/latest/installing.html
 and the sound chip is a DY-SV5W
+
+## Flashing
+
+Use the ESP32 firebettle board to flash the chip and use windows if possible 
+
+## Importanten notes
+
+1. Check if the IP of the lught computer is correct (on light computer use the command ipconfig window+r=> CMD)
+    - If not change in code and flash again
+
+2. ensure that both (chip and light computer are the in same wifi (fischnetz))
+    - If not change in code and flash again
+2. On light computer ensure the following settings are made in magicQ.
+    - Go to network > set your ip address
+    - Go to setup > dmx/IO enable Universe 7 (Artnet out 6)
+    - Go tot the marco window > automation > insert a new automation
+3. Make the following automtion settings
+    - Input: DMX_input
+    - set channel and universe: 7-4
+    - mode: toggle
+
+
+
+
--- masterSwitch/masterSwitch.ino
+++ masterSwitch/masterSwitch.ino
@@ -1,10 +1,26 @@
-#include <FastLED.h>
+/*
+ * 
+ * 
+ * This project is the attic switch and controls the a light slow via a DMX signal
+*/
+
 #include <ArtnetWifi.h>
+#include <FastLED.h>
 #include <Arduino.h>
 
+// ===================== IMPORTANT CONSTANTS COULD BE CAHNGED =====================
+//Wifi settings
+const char *ssid = "Fischnetz";     // CHANGE FOR YOUR SETUP
+const char *password = "incubator"; // CHANGE FOR YOUR SETUP
+const int startUniverse = 6;        // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as 0.
+const char host[] = "192.168.1.60"; // CHANGE FOR YOUR SETUP your destination
+
+// ===================== IMPORTANT CONSTANTS COULD BE CAHNGED =====================
+
+// after this line change only if you know what you are doing
 // pin connected to the on/off state of the switch
-const int switchOffPin= 12;     
-const int switchOnPin = 13;     
+const int switchOffPin = 12;
+const int switchOnPin = 13;
 
 // status led params
 const int statusLedPin = 14;
@@ -12,158 +28,189 @@ const int numStatusLeds = 4;
 CRGB statusLeds[numStatusLeds];
 
 // strobo led params
-const int stroboLedPin = 2;
+const int stroboLedPin = 2; //
 const int numStroboLed = 21;
 CRGB stroboLeds[numStroboLed];
-     
-// pin to send the sound signal
-const int soundPin =  15;     
 
-int buttonOffState = 1;         
-int buttonOnState = 0;  
+// pin to send the sound signal
+const int soundPin = 15;
+
+int buttonOffState = 1;
+int buttonOnState = 0;
 bool booting = false;
 
-//Wifi settings
-const char* ssid = "Fischnetz";
-const char* password = "incubator";
+//Poti settings
+// Analog pin
+const int potiPins[] = {
+    32,
+    34,
+    35};
+
+uint8_t lastPotiValues[] = {
+    0, 0, 0};
+
+// the dead band for poti values
+int MAX_DIFFERENCE_POTI_VALLUES = 5;
+int NR_POTIS = 3;
 
 // Artnet settings
 ArtnetWifi artnet;
-const int startUniverse = 6; 
-// ip of the target 
-const char host[] = "192.168.1.29";
-
-
-// set up function
-void setup() {
-  // set-up serial for debug output
-  Serial.begin(115200);
-  delay(1000);
-  Serial.print("Starting up...");
-  ConnectWifi();
-
-  // art net init
-  artnet.begin(host);
-  artnet.setLength(3); // check this param
-  artnet.setUniverse(startUniverse);
-  
-  // initialize the sound pin as an output:
-  pinMode(soundPin, OUTPUT);
-  pinMode(soundPin, OUTPUT);
-  // initialize the switch pin as an input:
-  pinMode(switchOnPin, INPUT);
-  pinMode(switchOffPin, INPUT);
-  // set up the leds
-  FastLED.addLeds<WS2812, statusLedPin, GRB>(statusLeds, numStatusLeds);
-  FastLED.addLeds<WS2812, stroboLedPin, GRB>(stroboLeds, numStroboLed);
-
-
-  // read the current state of the switch
-  buttonOnState = digitalRead(switchOnPin);
-  buttonOffState = digitalRead(switchOffPin);
-  
-  //switch sound off
-  digitalWrite(soundPin, HIGH);
-}
-
-
-void loop() {
-  int preButtonOffState = buttonOffState; 
-  
-  // read the state of the pushbutton value:
-  buttonOnState = digitalRead(switchOnPin);
-  buttonOffState = digitalRead(switchOffPin);
-
-  // switch was at off state an started moving to on state
-  if (preButtonOffState == HIGH && buttonOffState == LOW) {
-    booting = true;
-  }
-  // switch reached the on state
-  if (buttonOnState  == HIGH || buttonOffState  == HIGH ) {
-    booting = false;
-  }
-
- 
-  
-  if (booting) {
-    digitalWrite(soundPin, LOW);
-    // turn on strobo effect
-    strobo();
-  } else {
-    // high means no sound
-    digitalWrite(soundPin, HIGH);   
-  }
-  // switch is off
-  if(buttonOffState == HIGH) {
-    // turn on all leds in red
-    setStatusLeds(CRGB::Red);
-  }
-  // switch is on
-  if(buttonOnState == HIGH) {
-    // turn on all leds in green
-    setStatusLeds(CRGB::Green);
-    Serial.println("Sending dmx signal");
-    // TODO: set the right value at the right channel
-    uint8_t i = 0;
-    uint8_t j = 128;
-    artnet.setByte(i, j);
-    artnet.write();
-   
-  }
-}
-
-void strobo(){
-  for(int i = 0; i < numStroboLed; i++) {
-    stroboLeds[i] = CRGB::Blue;
-    FastLED.show();   
-  }
-  delay(100);
-  for(int i = 0; i < numStroboLed; i++) {
-    stroboLeds[i] = CRGB::Black;
-    FastLED.show();   
-  }
-  delay(100);
-}
-
-// set the status leds to a color
-void setStatusLeds(CRGB color) {
-  for(int i = 0; i < numStatusLeds; i++) {
-    statusLeds[i] = color;
-    FastLED.show();   
-  }
-}
 
 // connect to wifi – returns true if successful or false if not
 boolean ConnectWifi(void)
 {
-  boolean state = true;
+  boolean connection_state = true;
   int i = 0;
 
   WiFi.begin(ssid, password);
   Serial.println("");
   Serial.println("Connecting to WiFi");
-  
+
   // Wait for connection
   Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
-    if (i > 20){
-      state = false;
+    if (i > 20)
+    {
+      connection_state = false;
       break;
     }
     i++;
   }
-  if (state) {
+  if (connection_state)
+  {
     Serial.println("");
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
-    Serial.println(IPAddress(WiFi.localIP()));
-  } else {
+    Serial.println(WiFi.localIP());
+  }
+  else
+  {
     Serial.println("");
     Serial.println("Connection failed.");
   }
-  
-  return state;
+
+  return connection_state;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // connect to wifi
+  ConnectWifi();
+
+  // connect to artnet
+  artnet.begin(host);
+  artnet.setLength(3);
+  artnet.setUniverse(startUniverse);
+
+  // initialize the sound pin as an output:
+  pinMode(soundPin, OUTPUT);
+  pinMode(soundPin, OUTPUT);
+
+  // initialize the switch pin as an input:
+  pinMode(switchOnPin, INPUT);
+  pinMode(switchOffPin, INPUT);
+
+  // set up the leds
+  FastLED.addLeds<WS2812, statusLedPin, GRB>(statusLeds, numStatusLeds);
+  FastLED.addLeds<WS2812, stroboLedPin, GRB>(stroboLeds, numStroboLed);
+
+  // read the current state of the switch
+  buttonOnState = digitalRead(switchOnPin);
+  buttonOffState = digitalRead(switchOffPin);
+
+  //switch sound off
+  digitalWrite(soundPin, HIGH);
+}
+
+void loop()
+{
+  // for every poti forward the value as a dmx signal
+  for (int i = 0; i < NR_POTIS; i++)
+  {
+    uint8_t normed_value = map(analogRead(potiPins[i]), 0, 4095, 0, 255);
+    if (abs(lastPotiValues[i] - normed_value) > MAX_DIFFERENCE_POTI_VALLUES)
+    {
+      artnet.setByte(i, normed_value);
+      lastPotiValues[i] = normed_value;
+    }
+  }
+
+  // DB Schalter
+  int preButtonOffState = buttonOffState;
+
+  // read the state of the pushbutton value:
+  buttonOnState = digitalRead(switchOnPin);
+  buttonOffState = digitalRead(switchOffPin);
+
+  // switch was at off state an started moving to on state
+  if (preButtonOffState == HIGH && buttonOffState == LOW)
+  {
+    booting = true;
+  }
+  // switch reached the on state
+  if (buttonOnState == HIGH || buttonOffState == HIGH)
+  {
+    booting = false;
+  }
+
+  if (booting)
+  {
+    digitalWrite(soundPin, LOW);
+    // turn on strobo effect
+    strobo();
+  }
+  else
+  {
+    // high means no sound
+    digitalWrite(soundPin, HIGH);
+  }
+  // switch is off
+  if (buttonOffState == HIGH)
+  {
+    // turn on all leds in red
+    setStatusLeds(CRGB::Red);
+    artnet.setByte(3, 0);
+  }
+  // switch is on
+  if (buttonOnState == HIGH)
+  {
+    // turn on all leds in green
+    setStatusLeds(CRGB::Green);
+    artnet.setByte(3, 255);
+  }
+
+  artnet.write();
+  delay(50);
+}
+
+void strobo()
+{
+  for (int i = 0; i < numStroboLed; i++)
+  {
+    stroboLeds[i] = CRGB::Blue;
+    FastLED.show();
+  }
+  delay(100);
+  for (int i = 0; i < numStroboLed; i++)
+  {
+    stroboLeds[i] = CRGB::Black;
+    FastLED.show();
+  }
+  delay(100);
+}
+
+// set the status leds to a color
+void setStatusLeds(CRGB color)
+{
+  for (int i = 0; i < numStatusLeds; i++)
+  {
+    statusLeds[i] = color;
+    FastLED.show();
+  }
 }
